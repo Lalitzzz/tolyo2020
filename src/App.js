1a18bb0032714d85956ece0373f56799f9457784
Lalitzzz <armyofficer01@gmail.com>
2024-08-05 10:08:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const[answer, setAnswer] = useState();
+  const[error, setError] = useState(null);
+  const[status, setStaus] = useState('typing');
+
+  if (status === 'success') 
+  {
+    return <h1>thats right</h1>
+  }
+
+    async function handleSubmit(e) {
+      e.preventDefault();
+      setStaus('submitting')
+      try {
+        await submitForm(answer);
+        setStaus('success');
+      } catch (err) {
+        setStaus('typing')
+        setError(err)
+      }
+    }
+
+    function handleTextareaChange(e) {
+      setAnswer(e.target.value);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <h2>City quiz</h2>
+       <p>who won the gold medal in tokyo olympics in javlin throw in 2020 japan</p>
+
+       <form onSubmit={handleSubmit}>
+        <textarea
+         value={answer}
+         onChange={handleTextareaChange}
+         disabled ={ status === 'submitting'}
+
+        />
+        <br/>
+        <button disabled ={status ==='submitting'}>
+          Submit
+        </button> 
+           {error !== null && 
+             <p>{error.message}</p>
+           }
+       </form>
     </div>
-  );
+  )
+  function submitForm(answer) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() =>{
+        let shouldError = answer.toLowerCase() !== 'niraj chopra';
+
+        if(shouldError) {
+          reject(new Error( 'good guess but dear its wrong you shold work hard to get good knowledge from lalit sharma'))
+
+        } else {
+          resolve();
+        }
+       },1500)
+    })
+  }
 }
 
-export default App;
+export default App
